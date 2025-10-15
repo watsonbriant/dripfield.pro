79253e8d99058089befc72a406e91b31a76987d7
Brian Watson <watson.briant@gmail.com>
2025-10-15 15:02:48 -0400

    dividers

--- src/components/PerformanceChart.tsx
+++ src/components/PerformanceChart.tsx
@@ -491,7 +491,7 @@ const PerformanceChart: React.FC<PerformanceChartProps> = ({ performances, selec
                   className={`${
                     index % 2 === 0 ? 'bg-primary' : 'bg-canvas'
                   } hover:bg-tertiary/40 transition-colors text-xs ${
-                    isHighlighted ? 'border border-secondary bg-tertiary/40' : ''
+                    isHighlighted ? 'bg-tertiary/40' : ''
                   } ${
                     selectedGroup && !isHighlighted ? 'opacity-30' : 'opacity-100'
                   }`}
