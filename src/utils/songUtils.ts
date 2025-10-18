a0bb8a90b589e4927b6e6fbfb78334d621f98035
Brian Watson <watson.briant@gmail.com>
2025-10-18 16:00:55 -0400

    fix

--- src/utils/songUtils.ts
+++ src/utils/songUtils.ts
@@ -8,3 +8,18 @@ export const cleanSongName = (songName: string): string => {
     .replace(/…/g, '...')
     .replace(/∆/g, 'a');
 };
+
+export const getRarityColor = (rarity: string): string => {
+  switch (rarity.toLowerCase()) {
+    case 'rare':
+      return 'text-red-500';
+    case 'uncommon':
+      return 'text-orange-500';
+    case 'common':
+      return 'text-green-500';
+    case 'frequent':
+      return 'text-blue-500';
+    default:
+      return 'text-gray-500';
+  }
+};
