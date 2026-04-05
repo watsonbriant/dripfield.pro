bee3073ba7d2412fd1ec47e3347b7c7a7ddfe3cd
Brian Watson <watson.briant@gmail.com>
2026-04-05 19:08:48 -0400

    7.5.35 - setlistgame

--- src/components/SetlistGame.tsx
+++ src/components/SetlistGame.tsx
@@ -31,7 +31,7 @@ interface SubmissionDetails {
 }
 
 export function SetlistGame() {
-  const [activeLeague] = useState("2026 Misc");
+  const [activeLeague] = useState("2026 Spring");
   const { user } = useAuth();
   const [activeSongSelectionShow, setActiveSongSelectionShow] = useState<GameShow | null>(null);
   const [showScoringModal, setShowScoringModal] = useState(false);
